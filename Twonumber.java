ee166901c5ba3ef8fa472b113b1b348d4010823f
Abdul ghaffar <bajwaghaffar269@gmail.com>
2024-08-25 15:52:40 +0500

    java codes

---
+++ Twonumber.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package twonumber;
+
+/**
+ *
+ * @author Hp
+ */
+public class Twonumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=5;
+        int b=6;
+        //int product=a*b;
+         int temp=a;
+         a=b;
+         b=temp;
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+    
+}
